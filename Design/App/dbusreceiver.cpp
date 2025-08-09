108fe93f1138389908d872e76ff11f78148fc113
JAEHONG LIM <leo129044@gmail.com>
2025-08-09 02:03:03 +0200

    update dbusreceiver.cpp

--- Design/App/dbusreceiver.cpp
+++ Design/App/dbusreceiver.cpp
@@ -1,21 +1,24 @@
 #include "dbusreceiver.h"
 #include <QDebug>
+#include <QDBusReply>
+#include <QDBusVariant>
 
 DBusReceiver::DBusReceiver(QObject *parent) : QObject(parent) {
-    // 각 D-Bus 서비스에 대한 인터페이스 생성
+    // Battery DBus Interface
     batteryIface = new QDBusInterface(
-        "org.example.BatteryService",   // 서비스 이름 (버튼.py가 제공하는 서비스명)
-        "/Battery",                     // 오브젝트 경로
-        "org.example.Battery",          // 인터페이스 이름
-        QDBusConnection::sessionBus(), // 버스 종류
+        "com.car.Battery",               // Python battery.py의 서비스 이름
+        "/com/car/Battery",              // 오브젝트 경로
+        "org.freedesktop.DBus.Properties", // Get 메서드는 Properties 인터페이스에서 제공
+        QDBusConnection::systemBus(),    // battery.py는 SystemBus 사용
         this
         );
 
+    // Vehicle Controller DBus Interface
     vehicleIface = new QDBusInterface(
-        "org.example.VehicleService",   // 서비스 이름 (controller.py가 제공하는 서비스명)
-        "/Vehicle",                     // 오브젝트 경로
-        "org.example.Vehicle",          // 인터페이스 이름
-        QDBusConnection::sessionBus(),
+        "org.piracer.VehicleController", // Python controller.py의 서비스 이름
+        "/org/piracer/VehicleController",// 오브젝트 경로
+        "org.piracer.VehicleInterface",  // 인터페이스 이름
+        QDBusConnection::sessionBus(),   // controller.py는 SessionBus 사용
         this
         );
 }
@@ -26,11 +29,16 @@ QString DBusReceiver::getBatteryStatus() {
         return QString();
     }
 
-    QDBusReply<QString> reply = batteryIface->call("GetStatus");
+    // Get("com.car.Battery", "Percentage") 호출
+    QDBusReply<QDBusVariant> reply =
+        batteryIface->call("Get", "com.car.Battery", "Percentage");
+
     if (reply.isValid()) {
-        return reply.value();
+        // QVariant로 꺼내고 int로 변환
+        int percentage = reply.value().variant().toInt();
+        return QString::number(percentage);
     } else {
-        qWarning() << "Battery GetStatus call failed:" << reply.error().message();
+        qWarning() << "Battery Get call failed:" << reply.error().message();
         return QString();
     }
 }
