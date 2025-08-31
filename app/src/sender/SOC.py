029d74b1c1eba193e3a40ebf93e278dde79102c7
Si Woo Lee <siwoo0408@kookmin.ac.kr>
2025-08-31 11:14:30 +0200

    Update SOC.py

--- app/src/sender/SOC.py
+++ app/src/sender/SOC.py
@@ -5,7 +5,7 @@ import dbus.mainloop.glib
 from gi.repository import GLib
 from piracer.vehicles import PiRacerStandard
 
-# [수정] D-Bus 규칙에 맞는 올바른 이름으로 다시 통일
+# Unified naming according to D-Bus rules
 BUS_NAME = 'org.piracer.Battery'
 OBJECT_PATH = '/org/piracer/Battery'
 INTERFACE = 'org.piracer.Battery'
@@ -32,14 +32,13 @@ class BatteryService(dbus.service.Object):
         self.monitor = monitor
         self.was_charging = self.monitor.get_current() > 0.1
 
-    # [추가] C++ 클라이언트의 초기값 요청을 처리하는 새로운 메소드
-    # (percentage, current) 튜플을 반환합니다.
+    # Added: Method for C++ client to request initial values
+    # Returns a tuple (percentage, current)
     @dbus.service.method(INTERFACE, in_signature='', out_signature='(id)')
     def GetInitialStatus(self):
         voltage = self.monitor.get_voltage()
         percentage = self.get_percentage_from_voltage(voltage)
         current = self.monitor.get_current()
-        print(f"GetInitialStatus called. Returning: {percentage}%, {current:.2f}A")
         return percentage, current
 
     @dbus.service.signal(INTERFACE, signature='d')
@@ -51,7 +50,7 @@ class BatteryService(dbus.service.Object):
         pass
 
     def get_percentage_from_voltage(self, voltage):
-        # 3셀(3S) LiPo 배터리 전압 기준
+        # Based on 3-cell (3S) LiPo battery voltage
         MIN_VOLTAGE = 9.0
         MAX_VOLTAGE = 12.6
         percentage = ((voltage - MIN_VOLTAGE) / (MAX_VOLTAGE - MIN_VOLTAGE)) * 100.0
@@ -62,20 +61,16 @@ class BatteryService(dbus.service.Object):
         is_charging = current > 0.1
 
         if is_charging != self.was_charging:
-            print(f"Charging status changed to {is_charging}. Emitting signal with current: {current:.2f}A")
             self.chargingStatusChanged(current)
             self.was_charging = is_charging
 
     def emit_percentage(self):
         voltage = self.monitor.get_voltage()
         percentage = self.get_percentage_from_voltage(voltage)
-        print(f"Emitting periodic percentage: {percentage}%")
         self.percentageChanged(percentage)
 
 
 if __name__ == '__main__':
-    print("🔋 Battery Monitor DBus Service Started (Hybrid Mode)")
-
     dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
     bus = dbus.SessionBus()
     name = dbus.service.BusName(BUS_NAME, bus)
@@ -98,4 +93,4 @@ if __name__ == '__main__':
                 loop.get_context().iteration(False)
             time.sleep(0.5)
     except KeyboardInterrupt:
-        print("\nStopping Battery Monitor...")
+        pass
